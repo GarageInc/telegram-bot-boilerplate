948d6bb952319a6302e95173f6305f533841af9e
garageinc <9-b-rinat@rambler.ru>
2025-10-18 00:20:44 +0300

    fix: upd state on refresh

--- bot/src/menus/ExistingUserStart.ts
+++ bot/src/menus/ExistingUserStart.ts
@@ -57,7 +57,7 @@ export const ExistingUserStart = ({
 			}
 
 			// Force immediate update
-			await broadcasterService.updateSession(async () => { await ctx.sendMenu("ExistingUserStart", { state: null }); }, String(ctx.from!.id));
+			await broadcasterService.updateSession(async () => { await ctx.sendMenu("ExistingUserStart", { state: ctx.menu.state }); }, String(ctx.from!.id));
 		})
 		.dynamic(async (ctx, range) => {
 			// Handle Referrals button only if user has referrals enabled
