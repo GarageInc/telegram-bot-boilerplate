00ab7d1193c20aeed0b137fab67ea99a3fd0ccff
garageinc <9-b-rinat@rambler.ru>
2025-10-17 23:43:23 +0300

    fix: shared folder installs

--- bot/drizzle.config.ts
+++ bot/drizzle.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "drizzle-kit";
 
 import { DATABASE_URL } from "../shared/env";
 
+console.log("DATABASE_URL ===> ", DATABASE_URL);
+
+if (!DATABASE_URL) {
+	throw new Error("DATABASE_URL is required");
+}
+
 export default defineConfig({
 	out: "./../shared/infra/database/drizzle",
 	schema: "./../shared/database/schema.ts",
