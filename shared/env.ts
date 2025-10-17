d758134192de7992effcbe9858996168b4dcf5e0
garageinc <9-b-rinat@rambler.ru>
2025-10-17 23:16:05 +0300

    fix: http port 3000

--- shared/env.ts
+++ shared/env.ts
@@ -29,7 +29,7 @@ export const ENABLE_WEBHOOKS = opt("ENABLE_WEBHOOKS") === "true";
 
 console.log("ENABLE_WEBHOOKS", ENABLE_WEBHOOKS);
 
-export const PORT = ENABLE_WEBHOOKS ? reqd("PORT", port => parseInt(port, 10)) : undefined;
+export const PORT = ENABLE_WEBHOOKS ? reqd("PORT", port => parseInt(port, 10)) : 3000;
 export const BOT_WEBHOOK_HOST = ENABLE_WEBHOOKS ? reqd("BOT_WEBHOOK_HOST") : undefined;
 export const WEBHOOK_SECRET_TOKEN = ENABLE_WEBHOOKS ? reqd("WEBHOOK_SECRET_TOKEN") : undefined;
 
